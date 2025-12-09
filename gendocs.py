deb056aead2a8d1cc915424610fc7f305e871d18
olavma <olavma2004@gmail.com>
2025-12-09 13:06:06 +0100

    CSS in pydoc fixed

--- gendocs.py
+++ gendocs.py
@@ -2,10 +2,15 @@
 
 import glob
 import os
+import importlib.resources
 
 """
 Generate the code documentation, using pydoc.
 """
+
+# El CSS no funcionava i buscant vaig trobar: https://stackoverflow.com/questions/79272347/how-do-i-format-pydoc-html-output
+# On, adaptant el codi, el css per fi ha funcionat
+css_data = importlib.resources.files('pydoc_data').joinpath('_pydoc.css').read_text()
 
 for dirname in ['generardataset', 'clustersciclistes']:
     flist = glob.glob('%s/*.py' % dirname)
@@ -16,4 +21,10 @@ for dirname in ['generardataset', 'clustersciclistes']:
             bname = os.path.splitext(os.path.basename(fname))[0]
             os.system('mv %s.html %s.%s.html' % (bname, dirname, bname))
     os.system('python -m pydoc -w %s' % dirname)
+    
+    # Per utilitzar el CSS
+    with open(dirname + ".html") as inp:
+        html = inp.read()
+    with open(dirname + ".html", "w") as out:
+        out.write(html.replace("</head>", "<style>%s</style></head>" % css_data))
 os.system('mv *.html docs')
